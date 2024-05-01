5422930dca09a49d72ab76e7b29e79d1851282d8
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-01 17:06:17 +0400

    Task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Some doc"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """Some doc"""
+    return (k, v)
