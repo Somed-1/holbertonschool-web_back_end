b682efaeffe26977c4b24d8e7e4a22b93f75736b
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-01 17:07:43 +0400

    Fix annotations

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Some doc"""
     return (k, v)
