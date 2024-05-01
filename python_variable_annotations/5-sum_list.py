0199ab392908ea56d8a5463de325980da8dc5d10
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-01 16:58:21 +0400

    Fix typing annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Some doc"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Some doc"""
     return sum(input_list)
