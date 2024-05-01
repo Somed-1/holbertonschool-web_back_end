5d8e4611f0043d05e1b5ee762c502a5551634675
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-01 17:03:48 +0400

    Fix wrong annotations

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Some doc"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Some doc"""
     return float(sum(mxd_lst))
