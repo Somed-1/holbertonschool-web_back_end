17822908670c9ef451dbac494de553776bb168a6
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-03 20:15:47 +0400

    Task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,4 +6,4 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Some doc"""
-    return wait_random(max_delay)
+    return asyncio.Task(wait_random(max_delay))
