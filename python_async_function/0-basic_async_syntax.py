86cbd2595f3643fc6fe5aff3a722cc919e375fb0
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-03 19:07:22 +0400

    Task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,10 @@
 """Some doc"""
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: Union[int, float]=10) -> Union[int, float]:
     rand_time = random.randrange(0, max_delay)
     await asyncio.sleep(rand_time)
     return rand_time
