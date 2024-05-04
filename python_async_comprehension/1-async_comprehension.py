40d4a34426518227eb2c4c987397c4928918cc96
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-04 21:06:17 +0400

    Task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Some doc"""
+import asyncio
+import random
+from typing import Generator, List
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Some doc"""
+    numbers = [i async for i in await async_generator()]
+    return numbers
