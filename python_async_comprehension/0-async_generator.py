b2ef17aa2f2a042f4b9a577a75f336c9a2044a03
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-04 21:00:18 +0400

    Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Some doc"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Some doc"""
     for i in range(10):
         await asyncio.sleep(1)
