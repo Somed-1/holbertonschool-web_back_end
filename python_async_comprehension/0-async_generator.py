bc7e1e2caff4b5cc239b9de0518712877540ee83
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-04 21:11:50 +0400

    Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     """Some doc"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randrange(0, 10)
+        yield random.uniform(0, 10)
