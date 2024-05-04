de0ca9176781efa8ec61d518aef8d25796752fc8
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-04 21:08:35 +0400

    Task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """Some doc"""
-    numbers = [i async for i in await async_generator()]
+    numbers = [i async for i in async_generator()]
     return numbers
