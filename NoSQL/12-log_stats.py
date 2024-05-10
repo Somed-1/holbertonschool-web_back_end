9aba8c95197ad83eab5b7ba74d07a5cd70adb6df
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-10 20:43:27 +0400

    Task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Something """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """ Something """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    length = logs_collection.count_documents({})
+    arr = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("{0} logs".format(length))
+    print("Methods:")
+    for method in arr:
+        lengths = logs_collection.count_documents({"method": method})
+        print("\tmethod {0}: {1}".format(method, lengths))
+    status = logs_collection.count_documents(
+        {
+            "method": "GET",
+            "path": "/status"
+        }
+    )
+    print("{0} status check".format(status))
