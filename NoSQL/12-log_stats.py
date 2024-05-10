3e661a76e466c5c339202e0f8de4dcfd00cb764b
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-10 20:44:32 +0400

    Fix something

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" Something """
+"""Find things"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """ Something """
+    """Find things"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
     length = logs_collection.count_documents({})
@@ -15,10 +15,7 @@ if __name__ == "__main__":
     for method in arr:
         lengths = logs_collection.count_documents({"method": method})
         print("\tmethod {0}: {1}".format(method, lengths))
-    status = logs_collection.count_documents(
-        {
-            "method": "GET",
-            "path": "/status"
-        }
-    )
+
+    status = logs_collection.count_documents({"method": "GET",
+                                              "path": "/status"})
     print("{0} status check".format(status))
