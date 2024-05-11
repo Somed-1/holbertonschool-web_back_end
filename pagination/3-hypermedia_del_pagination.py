50906892f35f518c669539990e29e55e4763cdd5
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-11 09:54:12 +0400

    Task 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ get_hyper_index method """
+        ds_len = len(self.dataset())
+        assert 0 <= index < ds_len
+        dataset = self.indexed_dataset()
+        pages = {}
+        tmp = index
+        while len(pages) < page_size and index < ds_len:
+            if index in dataset:
+                pages[index] = dataset[index]
+            index += 1
+
+        page_list = list(pages.values())
+        length = len(page_list)
+        keys = pages.keys()
+        return {
+            "index": tmp,
+            "next_index": max(keys) + 1,
+            "page_size": length,
+            "data": page_list
+        }
