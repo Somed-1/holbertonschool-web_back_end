c6f75b88786627776e657385425a4ff466dd38f3
Somed-1 <abbasovmaqa159@gmail.com>
2024-05-20 21:16:21 +0400

    Task 1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const classArray = [];
+
+  for (const size of sizes) {
+    classArray.push(new ClassRoom(size));
+  }
+  return classArray;
+}
